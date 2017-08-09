f08907a2c7d9e306a6bdd6d4bbc4757e9406dc35
Bably Kumari and Kelsey Langlois <kelsey@>
2017-08-09 14:59:26 -0700

    add js to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
-  $("#survey").submit(function(event) {
+  $("#order-form").submit(function(event) {
     var name = $("input#name").val();
+    var email = $("input#email").val();
+    var address = $("textarea#address").val();
+    var brand = $("input:radio[name=shoe]:checked").val();
+    var size = $("select#size").val();
     var color = $("input#color").val();
 
-    $(".name").text(name);
-    $(".submitted").css("color", color);
 
-    $("#survey").slideToggle();
+    $(".name").text(name);
+    $(".email").text(email);
+    $(".address").text(address);
+    $(".brand").text(brand);
+    $(".size").text(size);
+    $(".color").text(color);
+    $("#order-form").slideToggle();
     $(".submitted").slideToggle();
     event.preventDefault();
   });
